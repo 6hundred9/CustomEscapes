0ed215cab458f216c96d4b2e09e8586cc0eb0f1d
Mert Ali Bayram <mertali.bayram01@dogaokullari.k12.tr>
2024-04-12 22:56:51 +0300

    Initial commit

---
+++ CustomEscapes/EventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Exiled.API.Enums;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
+using Respawning;
+
+namespace CustomEscapes
+{
+    public class EventHandler
+    {
+        public static void Escaping(EscapingEventArgs ev)
+        {
+            if (ev.Player.Role.Side == Side.ChaosInsurgency && ev.Player.IsCuffed)
+            {
+                ev.IsAllowed = true;
+                ev.EscapeScenario = EscapeScenario.CuffedClassD;
+                ev.RespawnTickets = new KeyValuePair<SpawnableTeamType, float>(SpawnableTeamType.NineTailedFox, 5);
+                ev.NewRole = RoleTypeId.NtfSpecialist;
+            } else if (ev.Player.Role.Side == Side.Mtf && ev.Player.IsCuffed)
+            {
+                ev.IsAllowed = true;
+                ev.EscapeScenario = EscapeScenario.CuffedScientist;
+                ev.RespawnTickets = new KeyValuePair<SpawnableTeamType, float>(SpawnableTeamType.ChaosInsurgency, 5);
+                ev.NewRole = RoleTypeId.ChaosConscript;
+            }
+        }
+    }
+}
---
+++ CustomEscapes/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Exiled.API.Features;
+
+namespace CustomEscapes
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "CustomEscapes";
+        public override string Author => "6hundred9";
+        public override string Prefix => "CE";
+        public override Version RequiredExiledVersion => new Version(8, 8, 0);
+        public override bool IgnoreRequiredVersionCheck => true;
+        public static Plugin Instance;
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Escaping += EventHandler.Escaping;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Escaping -= EventHandler.Escaping;
+            base.OnDisabled();
+        }
+    }
+}
